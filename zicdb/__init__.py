43ce84796520bf225417f12e1eb9b0e5fd458516
fab <fab@gnux.info>
2007-12-28 07:58:10 +0100

    add support for "use" command

--- zicdb/__init__.py
+++ zicdb/__init__.py
@@ -3,9 +3,17 @@
 # ./run.py search #shak# in '@filename@L' and #but# in '@filename@L'
 
 def startup(action='help'):
+    import os
     import sys
     import zicdb.zshell as zshell
     if len(sys.argv) > 1:
+        action = sys.argv[1]
+
+    if '-' in sys.argv[0]:
+        os.environ['ZDB'] = sys.argv[0].split('-', 1)[1]
+    if action == 'use':
+        os.environ['ZDB'] = sys.argv[2]
+        del sys.argv[1:3] # Remove "use <db>"
         action = sys.argv[1]
 
     zshell.init()
