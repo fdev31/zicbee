15ab114522817c78ca48fd5989ad6d260664efd5
fab <fab@gnux.info>
2008-01-15 00:44:25 +0100

    Better documented

--- zicdb/mpctl.py
+++ zicdb/mpctl.py
@@ -1,25 +1,47 @@
+import os
 import select
 import subprocess
 
 class MPlayer(object):
     """ A class to access a slave mplayer process
-    you may want to use _command directly or call populate()
+    you may want to use command(name, args*) directly
+    or call populate() to access functions (and minimal doc).
+
+    Exemples:
+        mp.command('loadfile', '/desktop/funny.mp3')
+        mp.command('pause')
+        mp.command('quit')
+
+    Note:
+        After a .populate() call, you can access an higher level interface:
+            mp.loadfile('/desktop/funny.mp3')
+            mp.pause()
+            mp.quit()
+
+        Beyond syntax, advantages are:
+            - completion
+            - minimal documentation
+            - minimal return type parsing
     """
+
+    exe_name = 'mplayer' if os.sep == '/' else 'mplayer.exe'
+
     def __init__(self):
         self._mplayer = subprocess.Popen(
-                ['mplayer', '-slave', '-quiet', '-idle'],
+                [self.exe_name, '-slave', '-quiet', '-idle'],
                 stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
         self._readlines()
 
     def _readlines(self):
         ret = []
         while any(select.select([self._mplayer.stdout.fileno()], [], [], 0.6)):
-#            if self._mplayer.poll():
-#                raise SystemExit()
             ret.append( self._mplayer.stdout.readline() )
         return ret
 
-    def _command(self, name, *args):
+    def command(self, name, *args):
+        """ Very basic interface [see populate()]
+        Sends command 'name' to process, with given args
+        """
         cmd = '%s%s%s\n'%(name,
                 ' ' if args else '',
                 ' '.join(repr(a) for a in args)
@@ -32,8 +54,14 @@ class MPlayer(object):
     @classmethod
     def populate(kls):
         """ Populates this class by introspecting mplayer executable """
-        mplayer = subprocess.Popen(['mplayer', '-input', 'cmdlist'],
+        mplayer = subprocess.Popen([kls.exe_name, '-input', 'cmdlist'],
                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        def args_pprint(txt):
+            lc = txt.lower()
+            if lc[0] == '[':
+                return '%s=None'%lc[1:-1]
+            return lc
 
         while True:
             line = mplayer.stdout.readline()
@@ -43,11 +71,12 @@ class MPlayer(object):
                 continue
             args = line.split()
             cmd_name = args.pop(0)
-            func_str = '''def _mp_func(self, *args):
+            arguments = ', '.join([args_pprint(a) for a in args])
+            func_str = '''def _populated_fn(self, *args):
             """%(doc)s"""
             if not (%(minargc)d <= len(args) <= %(argc)d):
                 raise TypeError('%(name)s takes %(argc)d arguments (%%d given)'%%len(args))
-            ret = self._command('%(name)s', *args)
+            ret = self.command('%(name)s', *args)
             if not ret:
                 return None
             if ret[0].startswith('ANS'):
@@ -57,14 +86,14 @@ class MPlayer(object):
                 except:
                     return val
             return ret'''%dict(
-                    doc = '%s(%s)'%(cmd_name, ', '.join(args)),
+                    doc = '%s(%s)'%(cmd_name, arguments),
                     minargc = len([a for a in args if a[0] != '[']),
                     argc = len(args),
                     name = cmd_name,
                     )
             exec(func_str)
 
-            setattr(MPlayer, cmd_name, _mp_func)
+            setattr(MPlayer, cmd_name, _populated_fn)
 
 if __name__ == '__main__':
     import sys
