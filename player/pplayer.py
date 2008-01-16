807f4e2e3c897fa64e2d515a4c93818d70d589e1
fab <fab@gnux.info>
2008-01-16 01:19:34 +0100

    introducing self.selected + code refactoring

--- player/pplayer.py
+++ player/pplayer.py
@@ -19,64 +19,10 @@ import urllib
 
 class PPlayer(object):
 
-    def toggle_pause(self, w):
-        self.player.pause()
-
-    def play_prev(self, w):
-        self.cur_song -= 1
-        if self.cur_song < 0:
-            raise IndexError()
-        self._play_selected()
-
-    def play_next(self, w):
-        self.cur_song += 1
-        self._play_selected()
-
-    def _play_selected(self):
-        self.player.loadfile(self.playlist[self.cur_song])
-
-    def _tick_generator(self):
-        while True:
-            try:
-                if self._running:
-                    pos = self.player.get_time_pos()
-                    if pos is None:
-                        try:
-                            self.play_next(None)
-                        except IndexError:
-                            self._running = False
-                            self.info_lbl.set_text('Not Playing.')
-                    else:
-                        meta = '\n'.join('%s: %s'%(k, v) for k, v in self.player.meta)
-                        if not meta:
-                            meta = self.playlist[self.cur_song]
-                        self.info_lbl.set_text(meta)
-                        total = self.player.get_time_length()
-                        print pos, total
-                        if total and total > 0:
-                            pos = total / pos
-                        else:
-                            pos %= 100
-                        print repr(pos)
-                        self.cursor.set_value(float(pos))
-            except Exception, e:
-                print "E:", str(e)
-            finally:
-                yield True
-
-    def validate_pattern(self, w):
-        params = {'pattern':w.get_text()}
-        uri = 'http://localhost:9090/?plain=1&' + urllib.urlencode(params)
-        self.playlist = [l.strip() for l in urllib.urlopen(uri).readlines()]
-        self.cur_song = 0
-        print "PLAY", self.playlist[self.cur_song]
-        self.player.loadfile(self.playlist[self.cur_song])
-        self._running = True
-
     def __init__(self):
         self.player = mp.MPlayer()
         self.playlist = []
-        self.player.cur_song = None
+        self.player.cur_song = -1
         gobject.timeout_add(1666, self._tick_generator().next)
         self._running = False
 
@@ -95,6 +41,62 @@ class PPlayer(object):
         self._wtree.signal_autoconnect(handlers)
         self.win.connect('destroy', gtk.main_quit)
         self.win.show()
+
+    def _tick_generator(self):
+        while True:
+            try:
+                if self._running:
+                    pos = self.player.get_time_pos()
+                    if pos is None:
+                        try:
+                            self.play_next(None)
+                        except IndexError:
+                            self._running = False
+                            self.info_lbl.set_text('Not Playing.')
+                    else:
+                        meta = '\n'.join('%s: %s'%(k, v) for k, v in self.player.meta)
+                        if not meta:
+                            meta = self.playlist[self.player.cur_song]
+                        self.info_lbl.set_text(meta)
+                        total = self.player.get_time_length()
+                        print pos, total
+                        if total and total > 0:
+                            pos = total / pos
+                        else:
+                            pos %= 100
+                        print repr(pos)
+                        self.cursor.set_value(float(pos))
+            except Exception, e:
+                print "E:", str(e)
+            finally:
+                yield True
+
+    def validate_pattern(self, w):
+        params = {'pattern':w.get_text()}
+        uri = 'http://localhost:9090/?plain=1&' + urllib.urlencode(params)
+        self.playlist = [l.strip() for l in urllib.urlopen(uri).readlines()]
+        self.player.cur_song = 0
+        self._play_selected()
+        self._running = True
+
+
+    def toggle_pause(self, w):
+        self.player.pause()
+
+    def play_prev(self, w):
+        self.player.cur_song -= 1
+        if self.player.cur_song < 0:
+            raise IndexError()
+        self._play_selected()
+
+    def play_next(self, w):
+        self.player.cur_song += 1
+        self._play_selected()
+
+    def _play_selected(self):
+        self.player.loadfile(self.selected)
+
+    selected = property(lambda self: self.playlist[self.player.cur_song] if self.player.cur_song != -1 else None)
 
 pp = PPlayer()
 gtk.main()
