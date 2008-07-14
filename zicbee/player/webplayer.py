3c68e81a413bd86d97e26040f9f7c2beec3dd98f
fab <fab@gnux.info>
2008-07-14 22:08:11 +0200

    fix position unit
    --HG--
    branch : xp-fab

--- zicbee/player/webplayer.py
+++ zicbee/player/webplayer.py
@@ -44,7 +44,7 @@ class PlayerCtl(object):
                 try:
                     try:
                         with self._lock:
-                            self.position = int(self.player.prop_stream_pos/100000)
+                            self.position = int(self.player.prop_stream_pos/10000)
                     except:
                         self.position = None
 
