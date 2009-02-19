da167dfa3abc3d7c1174ddd85b7eac292f3047d2
fab <fab@gnux.info>
2009-02-19 22:30:09 +0100

    run.py: add support for embedded eggs

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
+import os
 import sys
+
+for path in ('%s%seggs/%s'%(os.curdir, os.path.sep, egg) for egg in os.listdir('eggs') if egg.endswith('.egg') or os.path.isdir(egg)):
+    sys.path.appendpath)
+
 from zicbee.core import startup
 startup()
