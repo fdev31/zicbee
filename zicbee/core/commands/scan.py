2cd6bb72dfc92d0db033c0d9b473283fb68b9cb7
fab <fab@gnux.info>
2009-06-10 01:56:01 +0200

    scan: adapt docstring to "fullhelp" format
    --HG--
    branch : wip

--- zicbee/core/commands/scan.py
+++ zicbee/core/commands/scan.py
@@ -22,9 +22,9 @@ def _scan(**kw):
 
 def do_inc_scan():
     """ Scan a directory in an incremental way, directory based:
-        - removed directories are flushed from DB
-        - added directories are scanned (if not empty)
-        NOTE: if you add a file into an already existing directory containing songs, it won't be detected!
+    - removed directories are flushed from DB
+    - added directories are scanned (if not empty)
+    NOTE: if you add a file into an already existing directory containing songs, it won't be detected!
     """
     directories = zshell.args + []
     print "Incremental scan asked for %s"%(', '.join(directories))
