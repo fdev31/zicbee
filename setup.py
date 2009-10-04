6b70d5cb2882f0513489314e4ad0aa8ecac253da
fab <fdev31@gmail.com>
2009-10-04 11:20:05 +0200

    don't handle version in a special way yet

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup (
         author='Fabien Devaux',
         author_email='fdev31@gmail.com',
         url = 'http://zicbee.gnux.info/',
-        download_url='http://zicbee.gnux.info/hg/index.cgi/zicbee/archive/%s.tar.bz2'%('default' if '-' in VERSION else VERSION),
+        download_url='http://zicbee.gnux.info/hg/index.cgi/zicbee/archive/%s.tar.bz2'%VERSION,
         license='BSD',
         platform='all',
         description='A simple & powerful distributed Music database engine',
