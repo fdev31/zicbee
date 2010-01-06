376a8497323935f8e0c2ad4b885e4a29d104f914
fab <fdev31@gmail.com>
2010-01-06 22:20:39 +0100

    Remove wasp from zicbee

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ Good luck !"""
 # also supported:
 #            'simplejson>=1.7.3',
 
-requirements = [ 'zicbee-lib>=0.5', 'buzhug>=1.5', 'mutagen>=1.14', 'web.py>=0.32' ]
+requirements = [ 'zicbee-lib>=0.6', 'buzhug>=1.5', 'mutagen>=1.14', 'web.py>=0.32' ]
 
 if sys.version_info[:2] < (2, 6):
     # add cjson dependency
@@ -70,7 +70,6 @@ setup (
             "console_scripts": [
                 'zicdb = zicbee.core:startup',
                 'zicserve = zicbee.core:serve',
-                'wasp = zicbee.wasp:startup',
                 ],
             "setuptools.installation" : [
                 'eggsecutable = zicbee.core:startup'
