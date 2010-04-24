104b6054bf604d916b713a5b7c219e40125cc5c6
fab <fdev31@gmail.com>
2010-04-24 12:47:12 +0200

    cleanup serve command handler

--- zicbee/core/commands/serve.py
+++ zicbee/core/commands/serve.py
@@ -1,37 +1,33 @@
 # vim: et ts=4 sw=4
+import os
+from zicbee.core.httpdb import kill_server
+from zicbee_lib.config import config
 
+def abort():
+    kill_server()
+    raise SystemExit()
 
-def do_serve(pure=False):
-    """ Create a ZicDB instance
-    parameters:
-        pure (default: False): just start DB serving, no player
-    """
-    # chdir to serve files at the right place
+def make_app(no_player=False, fork=None):
     import web
-    import socket
     import signal
-    import os, sys
-    from zicbee_lib.config import config
-    from zicbee_lib.debug import debug_enabled, DEBUG
+    from zicbee_lib.debug import debug_enabled
     from zicbee_lib.resources import resource_filename
-    from zicbee.core.httpdb import web_db_index, kill_server
+    from zicbee.core.httpdb import web_db_index
 
-    def abort():
-        kill_server()
-        raise SystemExit()
-
+    # Handle ^C keystroke (try atexit if any problem...)
     signal.signal(signal.SIGINT, abort)
 
-    if not pure:
+    if not no_player:
         # let's do webplayer
         try:
             from zicbee.core.httpplayer import webplayer
         except (ImportError, RuntimeError):
             print "Can't load webplayer, falling-back to pure db mode"
             DEBUG()
-            pure = True
+            no_player = True
 
     try:
+        # chdir to serve files at the right place
         p = os.path.dirname(resource_filename('zicbee.ui.web', 'static'))
         os.chdir( p )
     except Exception:
@@ -40,7 +36,7 @@ def do_serve(pure=False):
     pid = 0 # if not forking, still execute children commands
     do_detach = False # do not try to detach by default
 
-    if config.fork and not debug_enabled:
+    if config.fork and not debug_enabled or fork is True:
         try:
             pid = os.fork()
             do_detach = True # fork succeded, try to detach
@@ -52,26 +48,43 @@ def do_serve(pure=False):
         if do_detach:
             os.setsid()
 
-        sys.argv = ['zicdb', '0.0.0.0:%s'%(config.default_port)]
+        print "%s listening on:"%('Song browser' if no_player else 'Song browser and player'),
+        if no_player:
+            urls = ('/db/(.*)', 'web_db_index',
+                    '/(.*)', 'web_db_index'
+                    )
+        else:
+            urls = ('/db/(.*)', 'web_db_index',
+                    '/(.*)', 'webplayer')
+
+        app = web.application(urls, locals())
+        return app
+
+def do_serve(pure=False):
+    """ Create a ZicDB instance
+    parameters:
+        pure (default: False): just start DB serving, no player
+    """
+    import socket
+    from zicbee_lib.debug import DEBUG
+    import sys
+    sys.argv = ['zicdb', '0.0.0.0:%s'%(config.default_port)]
+
+    try:
+        make_app(no_player=pure).run()
+    except SystemExit:
+        print "ciao!"
+    except socket.error:
+        print "Already running!"
         try:
-            print "%s listening on:"%('Song browser' if pure else 'Song browser and player'),
-            if pure:
-                urls = ('/db/(.*)', 'web_db_index',
-                        '/(.*)', 'web_db_index'
-                        )
-            else:
-                urls = ('/db/(.*)', 'web_db_index',
-                        '/(.*)', 'webplayer')
-
-            app = web.application(urls, locals())
-            app.run()
-        except SystemExit:
-            print "ciao!"
-        except socket.error:
-            print "Already running!"
-        except:
-            DEBUG()
+            abort()
+        finally:
+            raise SystemExit()
+    except:
+        DEBUG()
+        try:
+            abort()
+        finally:
             print os.kill(os.getpid(), 9)
-            #print 'kill', os.getpid()
-
+        #print 'kill', os.getpid()
 
