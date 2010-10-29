beb6beaa7a0f3c3907fb23484c6286ce773d369a
fab <fdev31@gmail.com>
2010-10-30 00:00:03 +0200

    fix setup script in case setuptools is not here

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 try:
 	import setuptools
 except ImportError:
-	from ez_setup import use_setuptools
+	from distribute_setup import use_setuptools
 	use_setuptools()
 from setuptools import setup, find_packages
 
